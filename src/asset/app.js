d34bb540d6ee4ba435b95fdd4c12cc5cb2c92eb5
Leonardo Santos Severino <leonardossev@gmail.com>
2020-02-08 16:57:28 -0300

    Adiciona gráfico de barras para número de penaltis

--- src/asset/app.js
+++ src/asset/app.js
@@ -6,6 +6,8 @@ async function loadApp() {
   const players = await response.json();
 
   fillTable(players);
+
+  mapBarByPenaltis(players);
 }
 
 function fillTable(players) {
@@ -58,4 +60,53 @@ function openModal(playerInfo) {
     playerPhoto.alt = `Foto do ${playerInfo.Name}`;
 
   }
+}
+
+function mapBarByPenaltis(players) {
+  const labels = players.map(player => {
+    return player.data.Name
+  });
+
+  const penaltis = players.map(player => {
+    return player.data.Penalties
+  });
+
+  const ctx = document.getElementById('barChart').getContext('2d');
+    
+  new Chart(ctx, {
+  type: 'bar',
+  data: {
+      labels,
+      datasets: [{
+          label: 'Nº of Penaltis',
+          data: penaltis,
+          backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+      }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
 }
